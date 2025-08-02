d17b4dca9404778d1edf23687f91dd9c5c1a5a5a
George Soteriou <george.soteriou.1997@gmail.com>
2025-08-02 14:30:48 +0200

    Update Admin.php

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -10,7 +10,7 @@ class Admin implements ISettings {
     private IConfig $config;
     private string $appName;
 
-    public function __construct(IConfig $config, string $appName) {
+    public function __construct(IConfig $config, string $appName = 'pdf_tagger') {
         $this->config = $config;
         $this->appName = $appName;
     }
@@ -27,7 +27,7 @@ class Admin implements ISettings {
         $template->assign('scan_folders', $this->config->getAppValue($this->appName, 'scan_folders', '/Scans'));
         $template->assign('available_tags', $this->config->getAppValue($this->appName, 'available_tags', 'Invoice, Receipt, Contract, Report'));
         
-        // Add the new setting for choosing the processing mode. Default to true (using the parser).
+        // Add the setting for choosing the processing mode. Default to true (using the parser).
         $template->assign('use_pdf_parser', $this->config->getAppValue($this->appName, 'use_pdf_parser', '1') === '1');
 
         Util::addScript($this->appName, 'admin');
@@ -35,12 +35,24 @@ class Admin implements ISettings {
         return $template;
     }
 
-    public function getSectionID(): string {
-        return 'pdf_tagger';
+    /**
+     * @return string the section ID, e.g. 'sharing'
+     */
+    public function getSection(): string {
+        return 'pdf_tagger'; // The unique ID for the settings section
     }
 
+    /**
+	 * @return string the human-readable name of the section
+	 */
+	public function getName(): string {
+		return 'PDF Auto Tagger'; // The name displayed in the settings navigation
+	}
+
+    /**
+     * @return int whether the form should be rather on the top or bottom of all list elements
+     */
     public function getPriority(): int {
-        return 50;
+        return 50; // Controls the order in the settings menu
     }
 }
-
